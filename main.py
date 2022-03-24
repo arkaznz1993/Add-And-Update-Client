031efc20177ed9c55e38c7fb8be4063349c89ade
arkaznz1993 <arkabhowmik@outlook.com>
2022-03-24 09:05:51 +0530

    Made changes to main.py for Google Cloud Functions.

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import constants
 from database import database_connection
 
 
-values = client_details_sheet.get_values(constants.INVOICE_SPREADSHEET_RANGE)
-Client.instantiate_from_spreadsheet(values)
+def main(data, context):
+    values = client_details_sheet.get_values(constants.INVOICE_SPREADSHEET_RANGE)
+    Client.instantiate_from_spreadsheet(values)
 
-database_connection.insert_clients(Client.convert_to_db_list())
-database_connection.connection.close()
+    database_connection.insert_clients(Client.convert_to_db_list())
+    database_connection.connection.close()
+
+
+if __name__ == '__main__':
+    main('', '')
